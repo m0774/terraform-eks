c20794df24434782e3427897d7a01da001a62486
m0774 <mottag17@gmail.com>
2025-04-22 14:35:23 -0300

    feat: IAM role Controller

--- variables.tf
+++ variables.tf
@@ -7,3 +7,8 @@ variable "project_name" {
   type        = string
   description = "Project name to be used to name the resources (Name tag)"
 }
+
+variable "oidc" {
+  type        = string
+  description = "HTTPS URL from OIDC provider of the EKS cluster"
+}
