11b6fa7d7cd9d5d75d66dd45fb2427daa0334dad
m0774 <mottag17@gmail.com>
2025-05-03 11:21:00 -0300

    refactor: adjusting tags

--- provider.tf
+++ provider.tf
@@ -13,16 +13,10 @@ terraform {
       version = "2.13.2"
     }
   }
-
-  backend "s3" {
-    bucket = "comunidadedevops-gm"
-    key    = "dev/terraform.tfstate"
-    region = "us-east-1"
-  }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
 
 provider "kubernetes" {
